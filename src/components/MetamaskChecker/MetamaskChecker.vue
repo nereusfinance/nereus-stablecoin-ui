d0d9177371c5c4da67944bf19e1e3e51254ca97d
LothricFin <drew@nereus.finance>
2022-02-16 14:42:42 +0200

    chore: delete console logs

--- src/components/MetamaskChecker/MetamaskChecker.vue
+++ src/components/MetamaskChecker/MetamaskChecker.vue
@@ -33,9 +33,6 @@ export default {
   methods: {
     async checkProvider() {
       const provider = await detectEthereumProvider();
-      console.log('--------------------');
-      console.log('provider', provider);
-      console.log('--------------------');
       if (!provider) {
         this.$store.commit("setPopupState", {
           type: "browser",
