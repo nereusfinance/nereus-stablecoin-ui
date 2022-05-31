2bca14ad36c43b8f1802c9cd7cdf85328e2a0d5a
MaisonT <ak@nereus.finance>
2022-05-31 15:34:28 +0300

    feat: remove status block to individual component

--- src/components/UiComponents/Status.vue
+++ src/components/UiComponents/Status.vue
@@ -1,114 +1,9 @@
 <template>
   <div class="transaction-status-block">
-    <div class="status-block" v-if="transactionPending !== 'finished' && statusText[0] === 'Deposit'">
-      <div class="icon-text">
-        <img
-          src="@/assets/images/icon-one.svg"
-          alt=""
-          class="status-icon"
-        />
-        <h1 style="color: white">{{ statusText[0] }}</h1>
-      </div>
-      <hr>
-      <div class="icon-text">
-        <img
-          src="@/assets/images/icon-two.svg"
-          class="status-icon"
-          alt=""
-        />
-        <h1>{{ statusText[1] }}</h1>
-      </div>
-    </div>
-    <div class="status-block" v-if="transactionPending !== 'finished' && statusText[0] === 'Approve'">
-      <div class="icon-text">
-        <img
-          src="@/assets/images/icon-one.svg"
-          alt=""
-          class="status-icon"
-          v-if="transactionPending !== 'withdraw'
-        && transactionPending !== 'pending withdraw' "
-        />
-        <img
-          src="@/assets/images/icon-done.svg"
-          alt=""
-          class="status-icon"
-          v-if="transactionPending === 'withdraw'
-          || transactionPending === 'pending withdraw'"
-        />
-        <h1 style="color: white">{{ statusText[0] }}</h1>
-      </div>
-      <hr
-        :class="{active: transactionPending === 'withdraw' || transactionPending === 'pending withdraw'}"
-      >
-
-      <div class="icon-text">
-        <img
-          src="@/assets/images/icon-two.svg"
-          class="status-icon"
-          v-if="transactionPending !== 'pending withdraw'"
-          :class="{active: transactionPending === 'withdraw'}"
-          alt=""
-        />
-        <img
-          src="@/assets/images/icon-done.svg"
-          class="status-icon"
-          v-if="transactionPending === 'pending withdraw'"
-          :class="{active: transactionPending === 'withdraw'}"
-          alt=""
-        />
-        <h1
-          :class="{active: transactionPending === 'withdraw'
-          || transactionPending === 'pending withdraw'}"
-        >
-          {{ statusText[1] }}
-        </h1>
-      </div>
-      <hr
-        :class="{active: transactionPending === 'pending withdraw'}"
-      >
-      <div class="icon-text">
-        <img
-          src="@/assets/images/icon-three.svg"
-          alt=""
-          class="status-icon"
-          :class="{active: transactionPending === 'pending withdraw'}"
-        />
-        <h1 :class="{active: transactionPending === 'pending withdraw'}"
-        >
-          {{ statusText[2] }}
-        </h1>
-      </div>
-    </div>
-
-<!--    Finished-->
-    <div class="status-block-finished" v-if="transactionPending === 'finished'">
-      <div class="icon-text">
-        <img
-          src="@/assets/images/icon-done.svg"
-          alt=""
-          class="status-icon"
-        />
-        <h1> {{ statusText[0] }}</h1>
-      </div>
-      <hr>
-      <div class="icon-text">
-        <img
-          src="@/assets/images/icon-done.svg"
-          alt=""
-          class="status-icon"
-        />
-        <h1>{{ statusText[1] }}</h1>
-      </div>
-      <hr v-if="statusText.length > 2">
-      <div class="icon-text" v-if="statusText.length > 2">
-        <img
-          src="@/assets/images/icon-done.svg"
-          alt=""
-          class="status-icon"
-        />
-        <h1>{{ statusText[2] }}</h1>
-      </div>
-    </div>
+    <StatusBlock
+      :transactionPending="transactionPending"
+      :statusText="statusText"
+    />
 
     <hr>
     <!--  Central block-->
@@ -126,8 +21,10 @@
 </template>
 
 <script>
+import StatusBlock from "@/components/UiComponents/StatusBlock";
 export default {
   name: "Status",
+  components: { StatusBlock },
   props: {
     statusText: {
       type: Array,
@@ -156,63 +53,7 @@ export default {
   flex-direction: column;
   align-items: center;
 
-  //Status block
-  .status-block, .status-block-finished {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    gap: 12px;
-    margin-right: auto;
-    margin-left: auto;
-    margin-bottom: 11px;
 
-    width: auto;
-    height: 16px;
-
-    .icon-text {
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      gap: 3px;
-    }
-
-    .status-icon {
-      &.active {
-        filter: brightness(100);
-      }
-    }
-    hr {
-      width: 24px;
-      height: 1px;
-      background: #606060;
-      margin-top: 0;
-      border-width: 0;
-
-      &.active {
-        background: white;
-      }
-    }
-    h1 {
-      font-weight: 400;
-      font-size: 12px;
-
-      text-align: center;
-      color: #8A8A8A;
-
-      &.active {
-        color: white;
-      }
-    }
-  }
-  .status-block-finished {
-    h1 {
-      color: white
-    }
-    hr {
-      background: white;
-    }
-  }
 
   hr {
     height: 1px;
