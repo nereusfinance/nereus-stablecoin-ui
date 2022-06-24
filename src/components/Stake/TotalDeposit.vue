ddfb552c50342accaf3933f7d81de7bc5b1c4a53
Spame-zee <andrew@nereus.finance>
2022-06-24 14:21:26 +0300

    feat:change-state-continue-button

--- src/components/Stake/TotalDeposit.vue
+++ src/components/Stake/TotalDeposit.vue
@@ -18,14 +18,17 @@
         class="deposit-btn"
         :class="{ notActive: actionType === 'Withdraw' }"
         @click="onClick('Deposit')"
+        :disabled="actionType === 'Withdraw'"
       >
         Deposit
       </button>
       <button
         class="withdraw-btn"
-        :class="{ notActive: actionType === 'Deposit' || isDisabled }"
+        :class="{
+          notActive: actionType === 'Deposit' || isDisabled,
+        }"
         @click="onClick('Withdraw')"
-        :disabled="isDisabled"
+        :disabled="isDisabled || actionType === 'Deposit'"
       >
         Withdraw
       </button>
@@ -35,6 +38,7 @@
 
 <script>
 import TokenIcon from "@/components/UiComponents/TokenIcon";
+
 export default {
   name: "TotalDeposit",
   data() {
@@ -95,6 +99,7 @@ export default {
     gap: 8px;
     margin-bottom: 26px;
   }
+
   p {
     font-size: 24px;
 
@@ -104,6 +109,7 @@ export default {
 
     gap: 2px;
   }
+
   .token-icon-wrap {
     width: 32px;
     height: 32px;
@@ -114,6 +120,7 @@ export default {
     display: flex;
     flex-direction: row;
   }
+
   .deposit-btn {
     cursor: pointer;
     margin-top: 18px;
@@ -136,15 +143,23 @@ export default {
       color: #8a8a8a;
       background-color: #353535;
     }
+    &:disabled:hover {
+      cursor: not-allowed;
+      color: #8a8a8a;
+      background-color: #353535;
+    }
   }
+
   .deposit-btn:hover {
     color: black;
     background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
       #e7fc6e;
   }
+
   .info-icon {
     width: 13.3px;
   }
+
   .withdraw-btn {
     cursor: pointer;
     margin-top: 18px;
@@ -161,11 +176,19 @@ export default {
 
     font-size: 14px;
     color: black;
+
     &.notActive {
       color: #8a8a8a;
       background-color: #353535;
     }
+
+    &:disabled:hover {
+      cursor: not-allowed;
+      color: #8a8a8a;
+      background-color: #353535;
+    }
   }
+
   .withdraw-btn:hover {
     color: black;
     background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
@@ -177,14 +200,17 @@ export default {
   .total-deposit-block {
     height: 186px;
     padding: 24px 16px 24px 16px;
+
     p {
       font-size: 20px;
     }
+
     .token-icon-wrap {
       width: 26px;
       height: 26px;
       margin-right: 6px;
     }
+
     .deposit-btn {
       width: 140px;
       margin-right: 8px;
@@ -192,6 +218,7 @@ export default {
       height: 40px;
       padding: 0 0 3px 0;
     }
+
     .withdraw-btn {
       width: 100px;
       height: 40px;
@@ -208,14 +235,17 @@ export default {
     margin-left: auto;
     margin-right: auto;
     margin-bottom: 8px;
+
     .info-icon {
       width: 20px;
     }
+
     h1 {
       font-weight: 400;
       font-size: 18px;
       margin-bottom: 16px;
     }
+
     p {
       font-weight: 400;
       font-size: 20px;
@@ -233,6 +263,7 @@ export default {
       align-content: center;
       align-items: center;
     }
+
     .deposit-btn {
       margin-top: 28px;
       width: 296px;
@@ -240,12 +271,14 @@ export default {
       margin-right: 0;
       border-radius: 24px;
     }
+
     .withdraw-btn {
       margin-top: 12px;
       width: 296px;
       height: 48px;
       border-radius: 24px;
     }
+
     .container-top {
       gap: 6px;
       font-size: 18px;
