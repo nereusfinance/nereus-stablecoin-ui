d01fe6bd5f3a460de0e3047343a881223d72c2b6
AntonHladkiy <105917252+Caeltrial@users.noreply.github.com>
2022-06-29 10:49:52 +0300

    fix : fixed value for nxusd amount

--- src/components/Stake/TotalDeposit.vue
+++ src/components/Stake/TotalDeposit.vue
@@ -3,34 +3,34 @@
     <p class="container-top">
       Total deposit
       <img
-        v-tooltip="'Your deposited NXUSD'"
-        alt=""
-        class="info-icon"
-        src="@/assets/images/icon-info.svg"
+          v-tooltip="'Your deposited NXUSD'"
+          alt=""
+          class="info-icon"
+          src="@/assets/images/icon-info.svg"
       />
     </p>
     <div class="container">
-      <TokenIcon token="NXUSD" />
+      <TokenIcon token="NXUSD"/>
       <p v-tooltip="balanceWithDecimals" class="info-tooltip">
         {{ balance }} NXUSD
       </p>
     </div>
     <div class="btns">
       <button
-        class="deposit-btn"
-        :class="{ notActive: actionType === 'Withdraw' }"
-        @click="onClick('Deposit')"
-        :disabled="actionType === 'Withdraw'"
+          :class="{ notActive: actionType === 'Withdraw' }"
+          :disabled="actionType === 'Withdraw'"
+          class="deposit-btn"
+          @click="onClick('Deposit')"
       >
         Deposit
       </button>
       <button
-        class="withdraw-btn"
-        :class="{
+          :class="{
           notActive: actionType === 'Deposit' || isDisabled,
         }"
-        @click="onClick('Withdraw')"
-        :disabled="isDisabled || actionType === 'Deposit'"
+          :disabled="isDisabled || actionType === 'Deposit'"
+          class="withdraw-btn"
+          @click="onClick('Withdraw')"
       >
         Withdraw
       </button>
@@ -57,19 +57,24 @@ export default {
   },
   computed: {
     balanceWithDecimals() {
+      const userData = this.$store.getters.getUserData;
+      const NXUSDByTier1 = Number(this.normalizeBNValues(userData[0][1]));
+      const NXUSDByTier2 = Number(
+          this.normalizeBNValues(userData[2].sub(userData[0][1]))
+      );
       return new Intl.NumberFormat("en-EN", {
         maximumSignificantDigits: 18,
       }).format(
-        parseFloat(
-          this.$ethers.utils.formatEther(this.$store.getters.getUserData[1])
-        )
+          parseFloat(
+              NXUSDByTier1 + NXUSDByTier2
+          )
       );
     },
     balance() {
       const userData = this.$store.getters.getUserData;
       const NXUSDByTier1 = Number(this.normalizeBNValues(userData[0][1]));
       const NXUSDByTier2 = Number(
-        this.normalizeBNValues(userData[2].sub(userData[0][1]))
+          this.normalizeBNValues(userData[2].sub(userData[0][1]))
       );
       const total = NXUSDByTier1 + NXUSDByTier2;
       return total.toFixed(2);
@@ -110,6 +115,7 @@ export default {
     gap: 8px;
     margin-bottom: 26px;
   }
+
   p {
     font-size: 24px;
 
@@ -151,18 +157,19 @@ export default {
       color: #8a8a8a;
       background-color: #353535;
     }
+
     &:disabled:hover {
       cursor: not-allowed;
       color: #8a8a8a;
       background: linear-gradient(0deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)),
-        #353535;
+      #353535;
     }
   }
 
   .deposit-btn:hover {
     color: black;
     background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
-      #e7fc6e;
+    #e7fc6e;
   }
 
   .info-icon {
@@ -195,14 +202,14 @@ export default {
       cursor: not-allowed;
       color: #8a8a8a;
       background: linear-gradient(0deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)),
-        #353535;
+      #353535;
     }
   }
 
   .withdraw-btn:hover {
     color: black;
     background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
-      #e7fc6e;
+    #e7fc6e;
   }
 }
 
@@ -210,6 +217,7 @@ export default {
   .total-deposit-block {
     height: 186px;
     padding: 24px 16px 24px 16px;
+
     p {
       font-size: 20px;
     }
